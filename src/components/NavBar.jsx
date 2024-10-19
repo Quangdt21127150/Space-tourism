a8739b1da35e8a8ba7f729f3d8e6831d44f262c5
Nguyễn Hoàng Nhật Quang <nhnquang21@clc.fitus.edu.vn.com>
2024-10-19 18:42:30 +0700

    Finish Technology page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -20,19 +20,19 @@ function Navbar({ windowWidth }) {
 
   const paths = {
     home: {
-      path: "/",
+      path: "/space-tourism-website/",
       name: "Home",
     },
     destination: {
-      path: "/destination",
+      path: "/space-tourism-website/destination",
       name: "Destination",
     },
     crew: {
-      path: "/crew",
+      path: "/space-tourism-website/crew",
       name: "Crew",
     },
     technology: {
-      path: "/technology",
+      path: "/space-tourism-website/technology",
       name: "Technology",
     },
   };
@@ -40,46 +40,28 @@ function Navbar({ windowWidth }) {
   return (
     <>
       <header className="flex justify-between min-w-screen items-center px-6 pt-6 z-40 md:pt-0 md:px-0 lg:pt-10">
-        <Link to="/" className="md:ml-10 lg:ml-14">
-          <LogoIcon
-            height={windowWidth >= 768 ? 48 : 40}
-            width={windowWidth >= 768 ? 48 : 40}
-          />
+        <Link to="/space-tourism-website/" className="md:ml-10 lg:ml-14">
+          <LogoIcon height={windowWidth >= 768 ? 48 : 40} width={windowWidth >= 768 ? 48 : 40}/>
         </Link>
-        <button
-          className="text-primary focus:outline-none md:hidden z-50"
-          onClick={handleMobileNavToggle}
-        >
-          {mobileNavOpen ? (
-            <CloseIcon className="mr-0.5 md:hidden" />
-          ) : (
-            <HamburgerIcon className="md:hidden" />
-          )}
+        <button className="text-primary focus:outline-none md:hidden z-50" onClick={handleMobileNavToggle}>
+          {mobileNavOpen 
+            ? <CloseIcon className="mr-0.5 md:hidden" />
+            : <HamburgerIcon className="md:hidden" />
+          }
         </button>
         {mobileNavOpen && (
-          <MobileNav
-            handleOverlayClick={handleOverlayClick}
-            handleMobileNavToggle={handleMobileNavToggle}
-          />
+          <MobileNav handleOverlayClick={handleOverlayClick} handleMobileNavToggle={handleMobileNavToggle} />
         )}
         <nav className="hidden md:flex md:justify-center md:bg-opaque md:w-112 lg:w-207 backdrop-blur-2xl">
           <ul className="uppercase text-primary flex items-center gap-9 lg:gap-13 font-secondary md:min-h-24">
-            {windowWidth >= 1440 && (
-              <div className="h-tiny w-118 bg-white opacity-25 absolute right-nav-accent"></div>
-            )}
+          {windowWidth >= 1440 && <div className="h-tiny w-118 bg-white opacity-25 absolute right-nav-accent"></div>}
             {Object.values(paths).map((path, index) => (
               <li key={path.name}>
-                <Link
-                  to={path.path}
-                  className={`py-9 tracking-nav text-sub lg:text-base font-primary-condensed ${
-                    path.path === location.pathname
-                      ? "border-b-3"
-                      : "hover:border-b-3 hover:border-hover"
-                  }`}
+                <Link 
+                  to={path.path} 
+                  className={`py-9 tracking-nav text-sub lg:text-base font-primary-condensed ${path.path === location.pathname ? "border-b-3" : "hover:border-b-3 hover:border-hover"}`}
                 >
-                  {windowWidth >= 1440 && (
-                    <span className="p-3 font-bold">{"0" + index}</span>
-                  )}
+                  {windowWidth >= 1440 && <span className="p-3 font-bold">{"0" + index}</span>}
                   {path.name}
                 </Link>
               </li>
--- src/components/navigation/MobileNav.jsx
+++ src/components/navigation/MobileNav.jsx
@@ -1,54 +1,25 @@
-import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-function MobileNav({ handleOverlayClick, handleMobileNavToggle }) {
+function MobileNav({handleOverlayClick, handleMobileNavToggle}) {
   return (
-    <div
-      className="absolute inset-0 flex justify-end z-40"
-      onClick={handleOverlayClick}
-    >
-      <nav
-        className="w-64 bg-white bg-opacity-5 backdrop-blur-2xl h-full p-8 pt-29 overflow-auto "
-        onClick={(e) => e.stopPropagation()}
+    <div className="absolute inset-0 flex justify-end z-40" onClick={handleOverlayClick}>
+      <nav 
+        className="w-64 bg-white bg-opacity-5 backdrop-blur-2xl h-full p-8 pt-29 overflow-auto " 
+        onClick={e => e.stopPropagation()}
       >
         <ul className="uppercase text-primary flex flex-col gap-8 font-secondary">
           <li>
-            <Link
-              to="/"
-              className="tracking-nav"
-              onClick={handleMobileNavToggle}
-            >
-              <span className="inline-block min-w-9 font-bold">00</span>Home
-            </Link>
+            <Link to="/space-tourism-website/" className="tracking-nav" onClick={handleMobileNavToggle}><span className="inline-block min-w-9 font-bold">00</span>Home</Link>
           </li>
           <li>
-            <Link
-              to="/destination"
-              className="tracking-nav"
-              onClick={handleMobileNavToggle}
-            >
-              <span className="inline-block min-w-9 font-bold">01</span>
-              Destination
-            </Link>
+            <Link to="/space-tourism-website/destination" className="tracking-nav" onClick={handleMobileNavToggle}><span className="inline-block min-w-9 font-bold">01</span>Destination</Link>
           </li>
           <li>
-            <Link
-              to="/crew"
-              className="tracking-nav"
-              onClick={handleMobileNavToggle}
-            >
-              <span className="inline-block min-w-9 font-bold">02</span>Crew
-            </Link>
+            <Link to="/space-tourism-website/crew" className="tracking-nav" onClick={handleMobileNavToggle}><span className="inline-block min-w-9 font-bold">02</span>Crew</Link>
           </li>
           <li>
-            <Link
-              to="/technology"
-              className="tracking-nav"
-              onClick={handleMobileNavToggle}
-            >
-              <span className="inline-block min-w-9 font-bold">03</span>
-              Technology
-            </Link>
+            <Link to="/space-tourism-website/technology" className="tracking-nav" onClick={handleMobileNavToggle}><span className="inline-block min-w-9 font-bold">03</span>Technology</Link>
           </li>
         </ul>
       </nav>
@@ -58,7 +29,7 @@ function MobileNav({ handleOverlayClick, handleMobileNavToggle }) {
 
 MobileNav.propTypes = {
   handleOverlayClick: PropTypes.func.isRequired,
-  handleMobileNavToggle: PropTypes.func.isRequired,
+  handleMobileNavToggle: PropTypes.func.isRequired
 };
 
 export default MobileNav;
